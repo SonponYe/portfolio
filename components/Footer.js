562c0b03e8d79371a7c21e712cfad43555e022d5
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-03-13 11:45:33 +0000

    small changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 export default function Footer(){
   return (
     <footer className="py-8 md:py-10 text-center text-sm md:text-base text-[var(--text)]/70">
-      <div className="container-wide">© {new Date().getFullYear()} Sonpon Ye — <a href="https://github.com/SonponYe-shuaChief" className="underline">GitHub</a></div>
+      <div className="container-wide">© {new Date().getFullYear()} Sonpon Ye-shua -  <a href="https://github.com/SonponYe-shuaChief" className="underline">GitHub</a></div>
     </footer>
   )
 }
+ 
