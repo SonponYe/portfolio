f61a4e883bd74ec638ffb9c8d644a72b7aa5f864
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-03-13 11:20:29 +0000

    removed something

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar(){
             <>
               <a href="#about" className="nav-link">About</a>
               <a href="#projects" className="nav-link">Projects</a>
-              <Link href="/projects" className="nav-link">Projects Page</Link>
+              <Link href="/projects" className="nav-link">Projects</Link>
               <Link href="/blog" className="nav-link">Blog</Link>
               <a href="#skills" className="nav-link">Skills</a>
               <a href="#contact" className="nav-link">Contact</a>
