f7ed9cd99b4432e7b3193c27f01579b9e9e7c6a7
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-03-14 15:00:09 +0000

    navbar change

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,7 +29,6 @@ export default function Navbar(){
           ) : (
             <>
               <a href="#about" className="nav-link">About</a>
-              <a href="#projects" className="nav-link">Projects</a>
               <Link href="/projects" className="nav-link">Projects</Link>
               <Link href="/blog" className="nav-link">Blog</Link>
               <a href="#skills" className="nav-link">Skills</a>
